914bfb0cc12d3267c72d60108fadb87998c753e7
Leonardo Yonamine <84002974+lenyon@users.noreply.github.com>
2025-07-04 23:31:37 -0300

    fix: AppliedJobRequest
    remocao de DTO inutil

--- src/main/java/vagabounds/dtos/application/AppliedJobRequest.java
+++
@@ -1,9 +0,0 @@
-package vagabounds.dtos.application;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record AppliedJobRequest(
-    @NotNull
-    Long candidateId
-) { }
